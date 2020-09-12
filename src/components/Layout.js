04f64710ba1f6c3efd721b263ff64916581fe6e4
jordanyatckoske <jordanyatckoske@gmail.com>
2020-09-11 23:06:13 -0500

    Favicon, simple animations, form validation fixes

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,5 +1,6 @@
 import React from "react"
 import { Helmet } from "react-helmet"
+import Tada from "react-reveal/Tada"
 import "../assets/styles/global.scss"
 // import Hirerite from "../assets/images/hirerite.svg"
 
@@ -13,6 +14,27 @@ const Layout = ({ children }) => {
           rel="stylesheet"
         />
         <title>Hirerite</title>
+        <link
+          rel="apple-touch-icon"
+          sizes="152x152"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#f72585" />
+        <meta name="msapplication-TileColor" content="#7209b7" />
+        <meta name="theme-color" content="#ffffff"></meta>
       </Helmet>
       {/* <nav className="navbar" role="navigation" aria-label="main navigation">
         <div className="container has-text-centered">
@@ -23,11 +45,13 @@ const Layout = ({ children }) => {
       </nav> */}
       <main className="container">{children}</main>
       <footer className="footer has-background-white">
-        <div className="content has-text-centered">
-          <p className="overline text-pink content">
-            &copy; Hirerite {new Date().getFullYear()}
-          </p>
-        </div>
+        <Tada delay={500}>
+          <div className="content has-text-centered">
+            <p className="overline text-pink content">
+              &copy; Hirerite {new Date().getFullYear()}
+            </p>
+          </div>
+        </Tada>
       </footer>
     </div>
   )
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import React, { useState, useRef } from "react"
+import { Fade, HeadShake } from "react-reveal"
 import BrowserProfiles from "../assets/images/browserProfiles.svg"
 // imort BrowserProfiles from "../assets/images/browserProfiles.js"
 // import LaptopSearch from "../assets/images/laptopSearch.svg"
@@ -52,26 +53,29 @@ export default function Home() {
   return (
     <Layout>
       {/* WE HIRE THE RIGHT PEOPLE */}
-      <div className="hero">
+      <div className="hero tablet-fullheight">
         <div className="hero-body columns is-multiline is-vcentered">
           <div className="column is-half columns is-multiline is-mobile has-text-centered-mobile has-text-left-tablet">
-            <div className="column is-full">
-              <h1 className="title is-1">
-                Looking to Hire? <br /> We can help.
-              </h1>
-              <p className="subtitle">
-                We build teams and help you hire the right people.
-              </p>
-            </div>
-            <div className="column is-full-mobile is-full-tablet is-half-desktop">
-              <button
-                onClick={scrollToCompanyForm}
-                className="button button-purple-gradient"
-              >
-                Build your team
-              </button>
-            </div>
-            {/* <div className="column is-full-mobile is-full-tablet is-half-desktop">
+            <Fade bottom cascade>
+              <div className="column is-full">
+                <h1 className="title is-1">
+                  Looking to Hire? <br /> We can help.
+                </h1>
+                <p className="subtitle">
+                  We build teams and help you hire the right people.
+                </p>
+              </div>
+            </Fade>
+            <Fade bottom delay={500}>
+              <div className="column is-full-mobile is-full-tablet is-half-desktop">
+                <button
+                  onClick={scrollToCompanyForm}
+                  className="button button-purple-gradient"
+                >
+                  Build your team
+                </button>
+              </div>
+              {/* <div className="column is-full-mobile is-full-tablet is-half-desktop">
               <button
                 onClick={scrollToCandidateForm}
                 className="button button-blue-gradient"
@@ -79,29 +83,35 @@ export default function Home() {
                 Start your career
               </button>
             </div> */}
+            </Fade>
           </div>
           <div className="column is-half has-text-centered-mobile has-text-right-tablet">
-            <StackedResumes />
+            <Fade right delay={750}>
+              <StackedResumes />
+            </Fade>
           </div>
         </div>
       </div>
 
       {/* TRUSTED HISTORY */}
-      <div className="section  my-5 has-text-centered-mobile has-text-left-tablet">
+      <div className="bg-circles section  my-5 has-text-centered-mobile has-text-left-tablet">
         <div className="container columns column-reverse-mobile is-multiline is-vcentered is-mobile">
           <div className="column is-full-mobile is-half-tablet has-text-centered-mobile has-text-left-tablet">
-            <TeamMembers />
+            <Fade left delay={500}>
+              <TeamMembers />
+            </Fade>
           </div>
-
           <div className="column is-full-mobile is-half-tablet columns is-multiline is-mobile">
-            <div className="column is-full">
-              <p className="overline">Trusted history</p>
-              <h1 className="title is-1">Our success story</h1>
-              <p className="subtitle">
-                We help companies recruit the best candidates for the positions
-                in IT, Marketing, HR, and Sales
-              </p>
-            </div>
+            <Fade bottom cascade>
+              <div className="column is-full">
+                <p className="overline">Trusted history</p>
+                <h1 className="title is-1">Our success story</h1>
+                <p className="subtitle">
+                  We help companies recruit the best candidates for the
+                  positions in IT, Marketing, HR, and Sales
+                </p>
+              </div>
+            </Fade>
           </div>
         </div>
       </div>
@@ -109,15 +119,21 @@ export default function Home() {
       {/* SOFTWARE */}
       <div className="section my-5">
         <div className="container columns is-multiline is-vcentered">
-          <div className="column is-half has-text-centered-mobile has-text-left-tablet">
-            <p className="overline">Our technology</p>
-            <h1 className="title is-1">Powered by our software</h1>
-            <p className="subtitle">
-              Know your candidates sooner to make better hiring decisions later.
-            </p>
-          </div>
+          <Fade bottom cascade>
+            <div className="column is-half has-text-centered-mobile has-text-left-tablet">
+              <p className="overline">Our technology</p>
+              <h1 className="title is-1">Powered by our software</h1>
+              <p className="subtitle">
+                Know your candidates sooner to make better hiring decisions
+                later.
+              </p>
+            </div>
+          </Fade>
+
           <div className="column is-half has-text-centered-mobile has-text-right-tablet">
-            <BrowserProfiles />
+            <Fade right delay={500}>
+              <BrowserProfiles />
+            </Fade>
           </div>
         </div>
       </div>
@@ -126,15 +142,19 @@ export default function Home() {
       <div ref={companyForm} className="section my-5">
         <div className="container columns column-reverse-mobile is-mobile is-multiline is-vcentered ">
           <div className="column is-full-mobile is-half-tablet has-text-centered-mobile has-text-left-tablet">
-            <DeskAgreement />
+            <Fade left ssrFadeout={true}>
+              <DeskAgreement />
+            </Fade>
           </div>
           <div className="column is-full-mobile is-half-tablet has-text-centered-mobile has-text-left-tablet">
-            <p className="overline">Let's connect</p>
-            <h1 className="title is-1">Looking to hire?</h1>
-            <p className="subtitle">
-              We can connect you with the top candidates searching for work at
-              companies like yours!
-            </p>
+            <Fade bottom ssrFadeout={true}>
+              <p className="overline">Let's connect</p>
+              <h1 className="title is-1">Looking to hire?</h1>
+              <p className="subtitle">
+                We can connect you with the top candidates searching for work at
+                companies like yours!
+              </p>
+            </Fade>
             <form
               onSubmit={handleSubmit}
               data-netlify="true"
@@ -146,58 +166,66 @@ export default function Home() {
                */}
               <input type="hidden" name="form-name" value="companyContact" />
               <input type="hidden" name="bot-field" />
-
-              <div className="field">
-                <label className="label has-text-left" htmlFor="name">
-                  Name
-                </label>
-                <div className="control">
-                  <input
-                    className="input"
-                    type="text"
-                    id="name"
-                    name="name"
-                    value={companyName}
-                    placeholder="John"
-                    required
-                    onChange={e => setCompanyName(e.target.value)}
-                  />
+              <Fade bottom cascade delay={400} ssrFadeout={true}>
+                <div className="field">
+                  <label className="label has-text-left" htmlFor="name">
+                    Name
+                  </label>
+                  <div className="control">
+                    <input
+                      className="input"
+                      type="text"
+                      id="name"
+                      name="name"
+                      value={companyName}
+                      placeholder="John"
+                      required
+                      onChange={e => setCompanyName(e.target.value)}
+                    />
+                  </div>
                 </div>
-              </div>
-
-              <div className="field">
-                <label className="label has-text-left" htmlFor="email">
-                  Email
-                </label>
-                <div className="control">
-                  <input
-                    className="input"
-                    type="email"
-                    id="email"
-                    name="email"
-                    value={companyEmail}
-                    placeholder="hired@gmail.com"
-                    required
-                    onChange={e => setCompanyEmail(e.target.value)}
-                  />
+              </Fade>
+              <Fade bottom cascade delay={750} ssrFadeout={true}>
+                <div className="field">
+                  <label className="label has-text-left" htmlFor="email">
+                    Email
+                  </label>
+                  <div className="control">
+                    <input
+                      className="input"
+                      type="email"
+                      id="email"
+                      name="email"
+                      value={companyEmail}
+                      placeholder="hired@gmail.com"
+                      required
+                      onChange={e => setCompanyEmail(e.target.value)}
+                    />
+                  </div>
                 </div>
-              </div>
-
-              <div className="mt-5">
-                <button className="button button-blue-gradient" type="submit">
-                  Build your team
-                </button>
-              </div>
+              </Fade>
+              <Fade bottom cascade delay={900} ssrFadeout={true}>
+                <div className="mt-5">
+                  <button className="button button-blue-gradient" type="submit">
+                    Build your team
+                  </button>
+                </div>
+              </Fade>
               {error ? (
-                <div className="subtitle has-text-danger">{error}</div>
+                <HeadShake>
+                  <div className="subtitle has-text-danger">{error}</div>
+                </HeadShake>
               ) : null}
               {submitted ? (
-                <div className="subtitle has-text-success">
-                  Thank you for contacting us, we will be in touch!
-                </div>
+                <Fade bottom>
+                  <div className="subtitle has-text-success">
+                    Thank you for contacting us, we will be in touch!
+                  </div>
+                </Fade>
               ) : null}
             </form>
           </div>
+          {/* </Fade> */}
         </div>
       </div>
 
